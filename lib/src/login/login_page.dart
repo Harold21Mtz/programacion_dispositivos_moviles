b64bf2f0a46eedcaf5acdd21a58951680b70271f
Harold21Mtz <hfmartinezc@ufpso.edu.co>
2023-03-22 17:06:44 -0500

    Se añade animaciones

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:delivery/src/utils/my_colors.dart';
-import 'package:delivery/main.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -15,64 +15,148 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
         body: Container(
       width: double.infinity,
-      child: Column(
+      child: Stack(
         children: [
-          _imageBanner(),
-          _textFieldEmail(),
-          _textFieldPassword(),
-          _buttonLogin(),
-          _textDontHaveAccount(),
+          Positioned(
+            child: _circleLogin(),
+            top: -100,
+            left: -100,
+          ),
+          Positioned(
+            child: _textLogin(),
+            top: 52,
+            left: 26,
+          ),
+          Column(
+            children: [
+              _lottieAnimation(),
+              _textFieldEmail(),
+              _textFieldPassword(),
+              _buttonLogin(),
+              _textDontHaveAccount()
+            ],
+          ),
         ],
       ),
     ));
   }
-}
 
-Widget _textDontHaveAccount() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Text(
-        'No tienes cuenta',
-        style: TextStyle(color: MyColors.primaryColor),
-      ),
-      SizedBox(
-        width: 9,
-      ),
-      Text(
-        'Registrate',
-        style: TextStyle(
-            fontWeight: FontWeight.bold, color: MyColors.primaryColor),
-      ),
-    ],
-  );
-}
-
-Widget _buttonLogin() {
-  return Container(
-    width: double.infinity,
-    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-    child: ElevatedButton(
-      onPressed: () {},
-      child: Text('INGRESAR'),
-      style: ElevatedButton.styleFrom(
-          primary: MyColors.primaryColor,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          padding: EdgeInsets.symmetric(vertical: 15)),
-    ),
-  );
-}
-
-Widget _imageBanner(){
-  return Container(
-    margin: EdgeInsets.only(
-      top: MediaQuery.of(context).size.height * 0.12,
-      bottom: MediaQuery.of(context).size.height * 0.15),
+  Widget _imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 0.12,
+          bottom: MediaQuery.of(context).size.height * 0.15),
       child: Image.asset(
         'assets/img/delivery.png',
         height: 200,
         width: 200,
       ),
     );
+  }
+
+  Widget _textFieldEmail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Correo Electrónico',
+            hintStyle: TextStyle(color: MyColors.primaryColorDart),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            prefixIcon: Icon(
+              Icons.email,
+              color: MyColors.primaryColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Contraseña',
+            hintStyle: TextStyle(color: MyColors.primaryColorDart),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            prefixIcon: Icon(
+              Icons.lock,
+              color: MyColors.primaryColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('INGRESAR'),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: MyColors.primaryColor,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            padding: const EdgeInsets.symmetric(vertical: 15)),
+      ),
+    );
+  }
+
+  Widget _textDontHaveAccount() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'No tienes Cuenta',
+          style: TextStyle(color: MyColors.primaryColor),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Text(
+          'Registrate',
+          style: TextStyle(
+              color: MyColors.primaryColor, fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  Widget _circleLogin() {
+    return Container(
+      width: 240,
+      height: 240,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: MyColors.primaryColor),
+    );
+  }
+
+  Widget _textLogin() {
+    return Text(
+      'LOGIN',
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 22,
+        fontFamily: 'NimbusSans',
+      ),
+    );
+  }
+
+  Widget _lottieAnimation() {
+    return Container(
+      margin: EdgeInsets.only(top: 150, bottom: 30),
+      child: Lottie.asset('assets/json/delivery.json',
+        width: 350, height: 200, fit: BoxFit.fill),
+    );
+  }
 }
